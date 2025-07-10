dfb1642b95adebf6e25924657b916d0006241adb
harsh1504660 <harshsmj1504@gmail.com>
2025-07-10 15:35:06 +0530

    final push

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import { Analytics } from '@vercel/analytics/react';
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
